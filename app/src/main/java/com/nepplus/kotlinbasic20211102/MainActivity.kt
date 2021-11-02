4ccfbe314e3cdca2d2312ea5edc357f523f6ddad
이아현 <suk03029@gmail.com>
2021-11-02 11:39:06 +0900

    Toast 띄워보기

--- app/src/main/java/com/nepplus/kotlinbasic20211102/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic20211102/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.kotlinbasic20211102
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +16,10 @@ class MainActivity : AppCompatActivity() {
             Log.e("메인화면에서 찍는 로그","로그e")
         }
 //  이 줄은 주석이 됨(ctrl+/)
-
+        btnToast.setOnClickListener {
+//            토스트 버튼이 눌리면 할일 따로 설정
+            Toast.makeText(this,"버튼이 눌렸습니다.",Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
